fc9477caf79d7a38e968c4c44925d4d0463bb4ad
Pg Gonni <p.gonni@hotmail.com>
2020-06-22 09:34:15 -0400

    fixed loop error in printPokemon()

--- pokedex.js
+++ pokedex.js
@@ -1,19 +1,28 @@
 async function gen1Pokemon() {
-    let gen1 = await axios.get('https://pokeapi.co/api/v2/pokemon');
-    let allPoke = gen1.data.pokemon_species;
+    let pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+    let allPoke = pokemon.data.results;
     return printPokemon(allPoke);
 }
 
-async function printPokemon(results) {
-    console.log(results)
+async function printPokemon(res) {
+    console.log(res[0])
     let allGen1 = [];
-    for (let i = 0; i <= 20; i++){ 
-        let pokemonName = results[i].name;
+    for (let i = 0; i <= 19; i++){ 
+        console.log(i)
+        let pokemonName = res[i].name;
         let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         let stateInfo = {modalShow: false, spriteFront: true};
         Object.assign(getInfo.data, stateInfo);
         allGen1.push(getInfo.data);
     };
+    // for (let pokemon of res){ 
+    //   console.log(pokemon)
+    //   let pokemonName = pokemon.name;
+    //   let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    //   let stateInfo = {modalShow: false, spriteFront: true};
+    //   Object.assign(getInfo.data, stateInfo);
+    //   allGen1.push(getInfo.data);
+    // };
     console.log(allGen1)
     return allGen1;
 }
