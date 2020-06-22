768c289343b8f2c5171a6640712ad873f549d5a3
Pg Gonni <p.gonni@hotmail.com>
2020-06-22 09:43:12 -0400

    changed regular loop to for loop

--- pokedex.js
+++ pokedex.js
@@ -7,22 +7,22 @@ async function gen1Pokemon() {
 async function printPokemon(res) {
     console.log(res[0])
     let allGen1 = [];
-    for (let i = 0; i <= 19; i++){ 
-        console.log(i)
-        let pokemonName = res[i].name;
-        let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        let stateInfo = {modalShow: false, spriteFront: true};
-        Object.assign(getInfo.data, stateInfo);
-        allGen1.push(getInfo.data);
-    };
-    // for (let pokemon of res){ 
-    //   console.log(pokemon)
-    //   let pokemonName = pokemon.name;
-    //   let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    //   let stateInfo = {modalShow: false, spriteFront: true};
-    //   Object.assign(getInfo.data, stateInfo);
-    //   allGen1.push(getInfo.data);
+    // for (let i = 0; i <= 19; i++){ 
+    //     console.log(i)
+    //     let pokemonName = res[i].name;
+    //     let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    //     let stateInfo = {modalShow: false, spriteFront: true};
+    //     Object.assign(getInfo.data, stateInfo);
+    //     allGen1.push(getInfo.data);
     // };
+    for (let pokemon of res){ 
+      console.log(pokemon)
+      let pokemonName = pokemon.name;
+      let getInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      let stateInfo = {modalShow: false, spriteFront: true};
+      Object.assign(getInfo.data, stateInfo);
+      allGen1.push(getInfo.data);
+    };
     console.log(allGen1)
     return allGen1;
 }
